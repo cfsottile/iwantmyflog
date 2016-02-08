ae034cc652f9263128d2ef6bfb1778726b53ca82
Cristian Sottile <cfsottile@gmail.com>
2016-02-08 19:21:47 -0300

    added percentage indicator

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 Bundler.require
 
-def links_at_url(url)
+def posts_ids_at_url(url)
 	doc = Nokogiri::HTML(`curl -s #{url}`)
 	doc.css("#list_photos_mosaic").children.map do |square|
 		square.children.first["href"].split("/").last
@@ -13,16 +13,17 @@ end
 
 def get_posts_ids
 	base_url = "http://www.fotolog.com/#{$username}/mosaic/"
-	links = []
+	posts_ids = []
 	page = 0
 	loop do
-		links.concat(links_at_url(base_url + (page * 30).to_s + "/"))
+		posts_ids.concat(posts_ids_at_url(base_url + (page * 30).to_s + "/"))
 		page += 1
 		if !valid_url?(base_url + (page * 30).to_s + "/")
 			break
 		end
 	end
-	links
+	$posts_amount = posts_ids.size
+	posts_ids
 end
 
 def download_post(post_id)
@@ -31,8 +32,6 @@ def download_post(post_id)
 	target_file = "#{post_dir}/www.fotolog.com/#{$username}/#{post_id}/index.html"
 
 	create_dir(post_dir)
-	
-	p "Downloading #{$username}/#{post_id}"
 	
 	commands = [
 		"cd #{post_dir}",
@@ -43,7 +42,6 @@ end
 
 def create_dir(*dir)
 	dir.each do |d|
-		p "Creating directory #{d}"
 		system("mkdir #{d}")
 	end
 end
@@ -61,6 +59,13 @@ def path_for_post(post_id)
 end
 
 def download_posts
+	puts "Downloading..."
+	puts "FYI:"
+	puts "* This operation may (will) take a few minutes to start, do not get mad if it get stucked at 0.0% a while."
+	puts "* Around 90%, it will take a few long minutes to finish. You will desperate, but trust me, it will end."
+	print "\r0.0% complete"
+	
+	$start_time = Time.now.to_i
 	posts_ids = get_posts_ids
 	threads = []
 	threads_amount = calculate_threads_amount(posts_ids)
@@ -70,6 +75,7 @@ def download_posts
 			posts.each do |post|
 				download_post(post)
 				create_post_link(post)
+				update_percentage
 			end
 		end
 	end
@@ -82,10 +88,32 @@ def calculate_threads_amount(posts_ids)
 	posts_ids.size > max_threads ? posts_ids.size / max_threads : 1
 end
 
-$username = ARGV[0] 
+def update_percentage
+	$mutex.synchronize do
+		$downloaded_posts += 1
+		print "\r#{format("%.2f", $downloaded_posts.to_f / $posts_amount * 100)}% complete"
+		if $downloaded_posts == $posts_amount
+			$end_time = Time.now.to_i
+			# $total_time = Time.at($end_time - $start_time).strftime("%M:%S")
+			$total_time = $end_time - $start_time
+			puts "\nDownloaded #{$posts_amount} posts in #{$total_time} seconds"
+		end
+	end
+end
+
+$username = ARGV[0]
+if $username.nil?
+	puts "Usage: ./#{$0} your_fotolog_username"
+	exit 1
+end
 $main_dir = "#{Time.now.to_i}-#{$username}"
 $resources_dir = "#{$main_dir}/resources"
 $posts_dir = "#{$main_dir}/posts"
 
+$mutex = Mutex.new
+$posts_amount
+$downloaded_posts = 0
+
+puts "Starting..."
 create_dir($main_dir, $resources_dir, $posts_dir)
 download_posts
